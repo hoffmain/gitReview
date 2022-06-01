e073898222766f67537326e71656863310f26341
hoffmain <elexia.hoffman@gmail.com>
2022-05-31 20:52:44 -0500

    2nd commit

--- src/gitReview/gitReview01.java
+++ src/gitReview/gitReview01.java
@@ -1,5 +1,11 @@
 package gitReview;
 
 public class gitReview01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("good evening");
+		System.out.println("farewell");
+	}
 
 }
